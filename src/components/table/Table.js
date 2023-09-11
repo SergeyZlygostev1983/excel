efcc070e0ac5021cd0b945632d3b246cc4c7de27
Sergey Zlygostev <zlygostev.sergey1983@gmail.com>
2023-09-11 15:49:34 +0400

    finish table resize functional

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,36 @@
 import {ExcelComponent} from '../../modules/ExcelComponent';
 import {createTable} from './table.template';
+import {resizeHandler} from './table.resize';
 
 export class Table extends ExcelComponent {
     static className = 'excel__table';
 
+    constructor($root) {
+        super($root, {
+            // listeners: ['click', 'mousedown', 'mousemove', 'mouseup'],
+            listeners: ['mousedown'],
+        });
+    }
+
     toHTML() {
         return createTable(20);
     }
+
+    // onClick() {
+    //     console.log('click');
+    // }
+
+    onMousedown(event) {
+        if (event.target.dataset.resize) {
+            resizeHandler(this.$root, event);
+        }
+    }
+
+    // onMousemove() {
+    //     console.log('mousemove');
+    // }
+
+    // onMouseup() {
+    //     console.log('mouseup');
+    // }
 }
