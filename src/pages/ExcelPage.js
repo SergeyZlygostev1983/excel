65da5df20cb6d8d147e6c4da5b3105d386fc65dd
Sergey Zlygostev <zlygostev.sergey1983@gmail.com>
2023-09-16 16:17:19 +0400

    finish routing

---
+++ src/pages/ExcelPage.js
@@ -0,0 +1,43 @@
+import {Page} from '../modules/Page';
+import {Excel} from '../components/excel/Excel';
+import {Formula} from '../components/formula/Formula';
+import {Header} from '../components/header/Header';
+import {Table} from '../components/table/Table';
+import {Toolbar} from '../components/toolbar/Toolbar';
+import {createStore} from '../modules/createStore';
+import {storage, debounce} from '../modules/utils';
+import {normalizeInitialState} from '../redux/initialState';
+import {rootReducer} from '../redux/rootReducer';
+
+function storageName(param) {
+    return 'excel:' + param;
+}
+
+export class ExcelPage extends Page {
+    getRoot() {
+        const params = this.params ? this.params : Date.now().toString();
+        const state = storage(storageName(params));
+        const store = createStore(rootReducer, normalizeInitialState(state));
+
+        const stateListener = debounce(state => {
+            storage(storageName(params), state);
+        }, 300);
+
+        store.subscribe(stateListener);
+
+        this.excel = new Excel({
+            components: [Header, Toolbar, Formula, Table],
+            store,
+        });
+
+        return this.excel.getRoot();
+    }
+
+    afterRender() {
+        this.excel.init();
+    }
+
+    destroy() {
+        this.excel.destroy();
+    }
+}
